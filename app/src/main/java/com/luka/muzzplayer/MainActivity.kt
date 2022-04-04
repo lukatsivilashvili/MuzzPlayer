f19192909c40ce9a026aa915267484b8761669d7
Luka Tsivilashvili <lutsivilashvili@tbcbank.com.ge>
2022-04-04 15:01:25 +0400

    feature/RuntimePermissions: fix item layout,colors and temporarily add runtime permissions

--- app/src/main/java/com/luka/muzzplayer/MainActivity.kt
+++ app/src/main/java/com/luka/muzzplayer/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.luka.muzzplayer
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -11,17 +18,23 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.shape.CornerFamily
 import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.snackbar.Snackbar
 import com.luka.muzzplayer.databinding.ActivityMainBinding
+import com.luka.muzzplayer.extensions.showSnackbar
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var layout: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        layout = binding.container
+        val view = binding.root
         setContentView(binding.root)
+        requestPermission(view)
         setUpNavbar()
     }
 
@@ -39,13 +52,61 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
         supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(R.color.bgNavbar)))
 
-        val radius = resources.getDimension(R.dimen.radius_large)
 
         val bottomNavigationViewBackground = navView.background as MaterialShapeDrawable
         bottomNavigationViewBackground.shapeAppearanceModel =
             bottomNavigationViewBackground.shapeAppearanceModel.toBuilder()
-                .setTopRightCorner(CornerFamily.ROUNDED, radius)
-                .setTopLeftCorner(CornerFamily.ROUNDED, radius)
+                .setTopRightCorner(CornerFamily.ROUNDED, resources.getDimension(R.dimen.radius_large))
+                .setTopLeftCorner(CornerFamily.ROUNDED, resources.getDimension(R.dimen.radius_large))
                 .build()
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Log.d("Permission: ", "Granted")
+            } else {
+                Log.d("Permission: ", "Denied")
+            }
+        }
+
+    private fun requestPermission(view:View){
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                layout.showSnackbar(
+                    view,
+                    getString(R.string.permission_granted),
+                    Snackbar.LENGTH_INDEFINITE,
+                    getString(R.string.ok)
+                ) {}
+            }
+
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) -> {
+                layout.showSnackbar(
+                    view,
+                    getString(R.string.permission_required),
+                    Snackbar.LENGTH_INDEFINITE,
+                    getString(R.string.ok)
+                ) {
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    )
+                }
+            }
+
+            else -> {
+                requestPermissionLauncher.launch(
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            }
+        }
     }
 }
