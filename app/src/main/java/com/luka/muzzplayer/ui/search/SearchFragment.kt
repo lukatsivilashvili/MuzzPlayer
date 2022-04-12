6c6eb8b1fee7a56ca4180fdaa981ec006c0076a4
luka <lukawivilashvili@gmail.com>
2022-04-12 23:10:08 +0400

    change/HiltRefactor: Add AppClass, AppModule and necessary annotations

--- app/src/main/java/com/luka/muzzplayer/ui/search/SearchFragment.kt
+++ app/src/main/java/com/luka/muzzplayer/ui/search/SearchFragment.kt
@@ -4,7 +4,9 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.luka.muzzplayer.base.BaseFragment
 import com.luka.muzzplayer.databinding.FragmentSearchBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SearchFragment : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding::inflate) {
 
     override fun initialize(inflater: LayoutInflater, container: ViewGroup?) {
