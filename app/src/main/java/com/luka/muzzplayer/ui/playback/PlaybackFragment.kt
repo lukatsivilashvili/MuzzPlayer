01df1287c97a2a5ffb8630d62e32311a5a44404e
luka <lukawivilashvili@gmail.com>
2022-04-15 00:24:00 +0400

    feature/PlaybackScreen: Add Layout file

---
+++ app/src/main/java/com/luka/muzzplayer/ui/playback/PlaybackFragment.kt
@@ -0,0 +1,12 @@
+package com.luka.muzzplayer.ui.playback
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.luka.muzzplayer.base.BaseFragment
+import com.luka.muzzplayer.databinding.FragmentPlaybackBinding
+
+class PlaybackFragment : BaseFragment<FragmentPlaybackBinding>(FragmentPlaybackBinding::inflate) {
+    override fun initialize(inflater: LayoutInflater, container: ViewGroup?) {
+
+    }
+}
---
+++ app/src/main/java/com/luka/muzzplayer/ui/playback/PlaybackViewModel.kt
@@ -0,0 +1,9 @@
+package com.luka.muzzplayer.ui.playback
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class PlaybackViewModel : ViewModel() {
+
+}
